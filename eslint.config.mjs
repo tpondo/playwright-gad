be2c72cedf143eb6918ae95a44c53498e442c9ea
tpondo <tomasz.pondo@gmail.com>
2025-01-03 13:03:02 +0100

    chore: eslint with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {ignores: ['package-lock.json',
-   ' playwright-report/**',
-    'test-results/**']},
-  {files: ["**/*.{ts}"]},
-  {languageOptions: { globals: globals.node }},
+  {
+    ignores: ['package-lock.json', ' playwright-report/**', 'test-results/**'],
+  },
+  { files: ['**/*.{ts}'] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   {
     rules: {
       'no-console': 'error',
-    }
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
   },
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
