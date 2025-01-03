fde72be10555031438961a348350c62ed44884c5
tpondo <tomasz.pondo@gmail.com>
2025-01-03 13:28:26 +0100

    chore: eslint with playwright

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginPlaywright from 'eslint-plugin-playwright';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -18,5 +19,18 @@ export default [
     },
   },
   ...tseslint.configs.recommended,
+  eslintPluginPlaywright.configs['flat/recommended'],
+  {
+    rules: {
+      'playwright/no-nested-step': 'off'
+    },
+    settings: {
+      playwright: {
+        globalAliases: {
+          test: ['setup'],
+        }
+      }
+    }
+  },
   eslintPluginPrettierRecommended,
 ];
