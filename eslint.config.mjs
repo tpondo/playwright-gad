4d55a0d062a667601acd586c3c382a994c18c829
tpondo <tomasz.pondo@gmail.com>
2025-01-03 12:45:06 +0100

    chore: eslint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,9 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {ignores: ['package-lock.json',
+   ' playwright-report/**',
+    'test-results/**']},
   {files: ["**/*.{ts}"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
