e1fcead9097ccd61f31195371489a1f88ca97f3d
tpondo <tomasz.pondo@gmail.com>
2025-01-13 09:55:54 +0100

    test: user factory

--- tests/smoke/user-register.spec.ts
+++ tests/smoke/user-register.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '../../fixtures/fixtures';
+import { randomUserData } from '../../src/factories/user/user.factory';
 import { RegisterUser } from '../../src/models/user/user.model';
 import { pageTitle } from '../../src/test-data/page-title/page-title.data';
-import { faker } from '@faker-js/faker/locale/en';
 
-test.describe('Register user', () => {
+test.describe('Verify register', () => {
   test(
     'register user with correct data',
     {
@@ -14,20 +14,11 @@ test.describe('Register user', () => {
       },
     },
     async ({ registerPage, loginPage, welcomePage }) => {
-      const registerUser: RegisterUser = {
-        userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
-        userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
-        userEmail: '',
-        userPassword: faker.internet.password(),
-      };
-      registerUser.userEmail = faker.internet.email({
-        firstName: registerUser.userFirstName,
-        lastName: registerUser.userLastName,
-      });
+      const registerUserData: RegisterUser = randomUserData();
 
       await registerPage.goto();
-      await registerPage.registerUser(registerUser);
-      await loginPage.login(registerUser);
+      await registerPage.registerUser(registerUserData);
+      await loginPage.login(registerUserData);
 
       const title = await welcomePage.title();
       expect(title).toContain(pageTitle.welcome);
@@ -44,15 +35,41 @@ test.describe('Register user', () => {
     },
     async ({ registerPage }) => {
       const expectedErrorText: string = 'Please provide a valid email address';
-      const registerUser: RegisterUser = {
-        userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
-        userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
-        userEmail: '@#!',
-        userPassword: faker.internet.password(),
-      };
+      const incorrectEmail: string = '!@#';
+      const registerUserData: RegisterUser = randomUserData();
+      registerUserData.userEmail = incorrectEmail;
 
       await registerPage.goto();
-      await registerPage.registerUser(registerUser);
+      await registerPage.registerUser(registerUserData);
+
+      await expect(registerPage.emailErrorText()).toHaveText(expectedErrorText);
+    },
+  );
+  test(
+    'not register user with incorrect data - email not provided',
+    {
+      tag: '@smoke',
+      annotation: {
+        type: 'documentation',
+        description: 'GAD-R03-04',
+      },
+    },
+    async ({ registerPage, page }) => {
+      const expectedErrorText: string = 'This field is required';
+      const registerUserData: RegisterUser = randomUserData();
+
+      await registerPage.goto();
+      await registerPage
+        .userFirstNameInput()
+        .fill(registerUserData.userFirstName);
+      await registerPage
+        .userLastNameInput()
+        .fill(registerUserData.userLastName);
+      await registerPage
+        .userPasswordInput()
+        .fill(registerUserData.userPassword);
+      await registerPage.registerButton().click();
+      await page.waitForLoadState();
 
       await expect(registerPage.emailErrorText()).toHaveText(expectedErrorText);
     },
